ce38b880ae39d56175fe9081f67ef2701a4f2d1a
1只冲鸭 <82013727+yinyin0916@users.noreply.github.com>
2024-04-27 22:55:27 +0000

    page outline

--- bg_remove.py
+++ bg_remove.py
@@ -4,38 +4,41 @@ from PIL import Image
 from io import BytesIO
 import base64
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
+st.set_page_config(layout="wide", page_title="Vis Your MoT Model!")
 
-st.write("## Remove background from your image")
-st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
-)
-st.sidebar.write("## Upload and download :gear:")
+st.write("## 👀Visualize your model performance")
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-# Download the fixed image
 def convert_image(img):
     buf = BytesIO()
     img.save(buf, format="PNG")
     byte_im = buf.getvalue()
     return byte_im
 
+PAGES = ['Video1', 'Video2']
+
+st.sidebar.write("## Upload and download :gear:")
+
+if 'page' not in st.session_state:
+    st.session_state.page = 1 
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload your model outcome", type=["txt"])
+
+page = st.sidebar.radio('Video you Want to Explore', PAGES, index=st.session_state.page)
+# st.write(f"{page}")
 
 def fix_image(upload):
     image = Image.open(upload)
-    col1.write("Original Image :camera:")
+    col1.write("Your Model MoT")
     col1.image(image)
 
     fixed = remove(image)
-    col2.write("Fixed Image :wrench:")
+    col2.write("? MoT")
     col2.image(fixed)
     st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
-
-
-col1, col2 = st.columns(2)
-my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+    st.sidebar.download_button("Download visualization", convert_image(fixed), "fixed.png", "image/png")
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
